f49ae4c1bd5b20c608612ff453635dcad10b9f49
Daniel Leinberger <d.leinberger@xdev-software.de>
2023-01-13 13:47:30 +0100

    Implemented the license doc

--- src/main/java/xdev/db/sybase/jdbc/SybaseJDBCMetaData.java
+++ src/main/java/xdev/db/sybase/jdbc/SybaseJDBCMetaData.java
@@ -1,5 +1,27 @@
 package xdev.db.sybase.jdbc;
 
+/*-
+ * #%L
+ * SqlEngine Database Adapter Sybase
+ * %%
+ * Copyright (C) 2003 - 2023 XDEV Software
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
 
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
@@ -11,7 +33,6 @@ import java.util.Arrays;
 import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
